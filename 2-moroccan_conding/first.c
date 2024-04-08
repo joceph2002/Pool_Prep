5db427d2ce717861c520675219aa567d81cc55ee
joceph2002 <youssefbarbara1@gmail.com>
2024-04-08 21:52:00 +0000

    8apr

---
+++ 2-moroccan_conding/first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	// i did [] so it can store +chararcters list of char
+	char name [] = "youssef";
+	int age = 19;
+	
+	printf("hada ana smiti %s o andi %d\n",name ,age);
+	printf("o %s kayt3alm c ",name);
+
+	return 0;
+
+
+
+}
+
+
+
+
+
+
